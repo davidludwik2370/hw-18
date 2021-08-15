a0a3be42ce67182fe2deb712348c3bbbd51f4b98
David Ludwik <davidludwik237@gmail.com>
2021-08-15 16:07:56 -0400

    schema/routes

---
+++ routes/api.js
@@ -0,0 +1,74 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+// getLastWorkout: /api/workouts
+// addExercise: "/api/workouts/" + id
+//createWorkout: "/api/workouts"
+//getWorkoutsInRange: /api/workouts/range
+
+//add exercise
+router.put("/api/workouts/:id", (req, res) => {
+  // console.log('called')
+  Workout.updateOne(
+    { _id: req.params.id}, 
+    { $push: { exercises: req.body } }
+  ).then(workoutData => {
+    res.json(workoutData);
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  });
+});
+
+
+
+//create workout
+router.post("/api/workouts", ({ body }, res) => {
+  Workout.create(body)
+    .then(workoutData => {
+      res.json(workoutData);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+//get exercises
+// router.get("/api/workouts", (req, res) => {
+//   Workout.find({})
+//     // .sort({ date: -1 })
+//     .then(dbTransaction => {
+//       res.json(dbTransaction);
+//     })
+//     .catch(err => {
+//       res.status(400).json(err);
+//     });
+// });
+
+//get last workout
+router.get("/api/workouts", (req, res) => {
+  Workout.find().sort({ _id: -1 }).limit(1)
+    // .sort({ date: -1 })
+    .then(workoutData => {
+      res.json(workoutData);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+//get range
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find().sort({ _id: -1 }).limit(7)
+    // .sort({ date: -1 })
+    .then(workoutData => {
+      res.json(workoutData);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.set('views', path.join(__dirname, 'public'));
+// app.set('view engine', 'html');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/stats', (req, res) => res.sendFile(path.join(__dirname, '/public/stats.html')));
+app.get('/exercise', (req, res) => res.sendFile(path.join(__dirname, '/public/exercise.html')));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
+  useNewUrlParser: true
+//   ,
+//   useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
+
+
